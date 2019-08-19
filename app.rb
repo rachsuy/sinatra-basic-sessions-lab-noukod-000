d8560e0db319e9c28468fbd83fe3d43ad8f712f6
Rachele Suy <rachelesuy@gmail.com>
2019-08-19 17:48:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "pass"
+    end
+    #get '/hey' do 
+    #    @session = session
+     # end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do 
+        @sessions=session
+        item=params["item"]
+        @sessions[:item] = item
+    end
 end
